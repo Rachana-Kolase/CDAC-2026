9590c6891f7d667d057558d2cc67dbe0f7aaa7f2
Rachana-Kolase <rachana.kolase.cmaug26@gmail.com>
2026-08-22 21:04:32 +0530

    Add files via upload

---
+++ Logic Building/Assignment 2/DataTypes.java
@@ -0,0 +1,43 @@
+//Print Datatype
+
+//Program to print DataTypes
+
+class DataTypes{
+public static void main(String[]args){
+byte a=10;
+short b=1000;
+int c=50000;
+long d=10000L;
+float e=10.5f;
+double f=99.99;
+char g='A';
+boolean h=true;
+
+System.out.println("byte:"+ a);
+System.out.println("short:"+ b);
+System.out.println("int:"+ c);
+System.out.println("long:"+ d);
+System.out.println("float:"+ e);
+System.out.println("double:"+ f);
+System.out.println("char:"+ g);
+System.out.println("boolean:"+ h);
+}
+}
+
+
+/*Output:
+byte:10
+short:1000
+int:50000
+long:10000
+float:10.5
+double:99.99
+char:A
+boolean:true*/
+
+
+
+
+
+
+
