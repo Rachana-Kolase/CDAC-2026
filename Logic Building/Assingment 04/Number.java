e922fc22c30b5f30369460ef813e09244138ef54
Rachana-Kolase <rachana.kolase.cmaug26@gmail.com>
2026-08-22 23:14:14 +0530

    Add files via upload

---
+++ Logic Building/Assingment 04/Number.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class PrintNumber{
+public static void main(String[]args){
+ 
+Scanner sc=new Scanner(System.in);
+
+System.out.println("Enter No:");
+int n=sc.nextInt();
+
+for(int i=1;i<=n;i++){
+
+ System.out.println(i);
+ }
+}
+}
+
+
+
