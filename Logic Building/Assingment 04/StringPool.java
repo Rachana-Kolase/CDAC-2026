3b90f4244b0fb98c4696084a5255581d6d69e8de
Rachana-Kolase <rachana.kolase.cmaug26@gmail.com>
2026-08-23 22:36:50 +0530

    Add files via upload

---
+++ Logic Building/Assingment 04/StringPool.java
@@ -0,0 +1,14 @@
+//String pool with intern() method
+
+public class StringPool{
+    public static void main(String[] args) {
+        String str1 = new String("hello");
+        String str2 = str1.intern();
+        String str3 = "hello";
+
+        System.out.println("Is str2 and str3 pointing to the same object? " + (str2 == str3));
+    }
+}
+
+
+//Output:Is str2 and str3 pointing to the same object? true
